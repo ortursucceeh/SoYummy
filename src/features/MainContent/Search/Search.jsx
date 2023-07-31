e8033492ad1558f6ba01d0eddf17891aa7525bcd
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-07-31 19:58:42 +0300

    add search component

--- src/features/MainContent/Search/Search.jsx
+++ src/features/MainContent/Search/Search.jsx
@@ -1,5 +1,15 @@
+import Button from '../../../ui/Button/Button';
+import styles from './Search.module.scss';
+
 function Search() {
-  return <div>Search</div>;
+  return (
+    <div className={styles.searchWrapper}>
+      <input type="text" className={styles.input} />
+      <Button type="curv" color="dark">
+        Search
+      </Button>
+    </div>
+  );
 }
 
 export default Search;
