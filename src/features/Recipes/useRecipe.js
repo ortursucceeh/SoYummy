2e3a3060ec3825fb4d9da7b526e53b5eabaf9552
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-08-05 17:28:45 +0300

    add useRecipe and basic behaviour

---
+++ src/features/Recipes/useRecipe.js
@@ -0,0 +1,15 @@
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+import { getRecipesById } from '../../services/apiRecipes';
+
+export function useRecipe() {
+  const { recipeId } = useParams();
+  console.log(recipeId);
+
+  const { isLoading, data: recipe } = useQuery({
+    queryKey: ['recipeById', recipeId],
+    queryFn: () => getRecipesById(recipeId),
+  });
+
+  return { recipe, isLoading };
+}
